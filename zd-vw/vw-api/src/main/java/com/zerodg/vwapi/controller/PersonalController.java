f2b12a3dc19061bc800d7fbe843b030a39ea7c5d
ChenPrince <1163005753@qq.com>
2020-08-21 22:20:57 +0800

    gitee--->github

--- zd-vw/vw-api/src/main/java/com/zerodg/vwapi/controller/PersonalController.java
+++ zd-vw/vw-api/src/main/java/com/zerodg/vwapi/controller/PersonalController.java
@@ -1,15 +1,14 @@
 package com.zerodg.vwapi.controller;
 
 import com.zerodg.serviceImpl.PersonalServiceImpl;
+import com.zerodg.vwentity.dto.Personal.DeleteArticleByArticleIdInputDTO;
 import com.zerodg.vwentity.dto.Personal.GetMyArticlesDTO;
 import com.zerodg.vwentity.dto.Personal.GetMyArticlesInputDTO;
 import com.zerodg.zdutil.util.JSONResult;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
-import org.springframework.beans.factory.annotation.Autowire;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -52,6 +51,15 @@ public class PersonalController {
         return jsonResult;
     }
 
-    //根据文章 id 删除文章
+    @ApiOperation(value="article id",notes="文章 id",produces = MediaType.APPLICATION_PROBLEM_JSON_UTF8_VALUE)
+    @RequestMapping(value="/deleteArticleByArticleId",method=RequestMethod.GET)
+    public JSONResult deleteArticleByArticleId(@Valid DeleteArticleByArticleIdInputDTO input){
+
+        System.out.println(input.getArticleId());
+
+        JSONResult jsonResult=new JSONResult();
+        return jsonResult;
+
+    }
 
 }
---
+++ zd-vw/vw-entity/src/main/java/com/zerodg/vwentity/dto/Personal/DeleteArticleByArticleIdInputDTO.java
@@ -0,0 +1,21 @@
+package com.zerodg.vwentity.dto.Personal;
+
+/**
+ * @program: eroot
+ * @description:
+ * @author: ztz-prince
+ * @create:
+ * @date:2019-10-29-00:12
+ **/
+public class DeleteArticleByArticleIdInputDTO {
+
+    private Integer articleId;
+
+    public Integer getArticleId() {
+        return articleId;
+    }
+
+    public void setArticleId(Integer articleId) {
+        this.articleId = articleId;
+    }
+}
